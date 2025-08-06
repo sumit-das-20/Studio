ae0a0163b5b8f7583a527f484c6a00f44ab78303
Sumit Das <sumittechdas@gmail.com>
2025-08-06 16:23:37 +0000

    I am saying first you create application name taskrabbit and create fire

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,10 +5,10 @@ import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
   projectId: 'taskbatao',
   appId: '1:99780414671:web:f50791766ba192894a7bba',
-  storageBucket: 'taskbatao.firebasestorage.app',
+  storageBucket: 'taskbatao.appspot.com',
   apiKey: 'AIzaSyB2RbPCV8JDmgp4wd8FNqjCiBM67KOSLHo',
   authDomain: 'taskbatao.firebaseapp.com',
-  measurementId: '',
+  measurementId: 'G-S1JNB92V1L',
   messagingSenderId: '99780414671',
 };
 
