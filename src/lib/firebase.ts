33ef468e6932382edaef0384aab5fac3cec69eea
Sumit Das <sumittechdas@gmail.com>
2025-08-06 16:15:22 +0000

    Application name changing not done. Please change it

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'taskrabbit-rewards',
+  projectId: 'taskbatao',
   appId: '1:99780414671:web:f50791766ba192894a7bba',
-  storageBucket: 'taskrabbit-rewards.firebasestorage.app',
+  storageBucket: 'taskbatao.firebasestorage.app',
   apiKey: 'AIzaSyB2RbPCV8JDmgp4wd8FNqjCiBM67KOSLHo',
-  authDomain: 'taskrabbit-rewards.firebaseapp.com',
+  authDomain: 'taskbatao.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '99780414671',
 };
